1d74898d10001102b64f6e57b5c3927a62c62533
seunghunHan1 <hkgb0009@gmail.com>
2022-11-02 13:00:32 +0900

    asdc

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  devServer: {
-    disableHostCheck: true,
-  },
+  devServer: {},
   transpileDependencies: true,
 });
